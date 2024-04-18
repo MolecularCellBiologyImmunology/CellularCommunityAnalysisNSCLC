4eb7ad2d2d26dada690ec0998643917c8266f9d1
jhroelink <jannes.roelink@gmail.com>
2024-04-18 14:42:21 +0200

    celltype cluster heatmao

--- Crick/clustmergefig_MOC2.R
+++ Crick/clustmergefig_MOC2.R
@@ -12,7 +12,7 @@ path = getwd()
 
 #dir.create(out_dir)
 
-clus = read.csv(paste0(path, "/Data/clustering/full_cd_CRICK_KO_clus_27_33_72.csv"))
+clus = read.csv(paste0(path, "/Data/clustering/full_cd_clus_28_41_56.csv"))
 
 # Cluster tree of neighbour clustering results 
 #clus$temp = clus$agglomerateto_49[match(neighb1$cluster, clus$cluster)]
@@ -21,13 +21,13 @@ clus = read.csv(paste0(path, "/Data/clustering/full_cd_CRICK_KO_clus_27_33_72.cs
 
 # Subset data and rename columns 
 subset = clus %>% 
-  select(cluster:agglomerateto_72) %>% 
-  dplyr::rename(Communities190 = cluster,
-                Communities72 = agglomerateto_72,
-                Communities33 = agglomerateto_33,
-                Communities27 = agglomerateto_27)
+  select(cluster:clusterstotal_56) %>% 
+  dplyr::rename(Communities306 = cluster,
+                Communities56 = clusterstotal_56,
+                Communities41 = clusterstotal_41,
+                Communities28 = clusterstotal_28)
 
 p = clustree(subset, prefix = "Communities", node_size=3, node_text_size = 2, node_text_angle = 60)
 ggsave(plot = p, device = "png", width=20, height=10, dpi=300, path = paste0(path, "/Data/plots/MOC2_CCR2KO/"),
-       filename = paste(date, "_clustree_CRICK_KO_clusters_n3_w30.png", sep = ""))
+       filename = paste(date, "_clustree_CRICK_MOC2_clusters_n3_w30.png", sep = ""))
 
