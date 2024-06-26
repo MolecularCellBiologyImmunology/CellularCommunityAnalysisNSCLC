6e6b1dd25b24efc68d361e9e95a93d11353fdfbc
jhroelink <jannes.roelink@gmail.com>
2024-06-26 14:24:10 +0200

    permutation

--- Sorin2/clustmergefig.R
+++ Sorin2/clustmergefig.R
@@ -10,10 +10,10 @@ date = format(Sys.Date(), "%Y%m%d")
 
 path = getwd()
 
-#out_fig1 = paste0(path, "/Data/full_cd_clus_20_40_49.csv"))
+#out_fig1 = paste0(path, "/Data/full_cd_clus_21_40_49.csv"))
 #dir.create(out_dir)
 
-clus = read.csv(paste0(path, "/Data/clustering/full_cd_clus_19_39_48.csv"))
+clus = read.csv(paste0(path, "/data/agglo_clus/aggloclus_clus18.csv"))
 
 # Cluster tree of neighbour clustering results 
 #clus$temp = clus$agglomerateto_49[match(neighb1$cluster, clus$cluster)]
@@ -22,13 +22,11 @@ clus = read.csv(paste0(path, "/Data/clustering/full_cd_clus_19_39_48.csv"))
 
 # Subset data and rename columns 
 subset = clus %>% 
-  select(cluster:agglomerateto_48) %>% 
-  dplyr::rename(Communities275 = cluster,
-                Communities48 = agglomerateto_48,
-                Communities39 = agglomerateto_39,
-                Communities19 = agglomerateto_19)
+  select(cluster:agglomerateto_18) %>% 
+  dplyr::rename(Communities31 = cluster,
+                Communities18 = agglomerateto_18)
 
 p = clustree(subset, prefix = "Communities", node_size=3, node_text_size = 2, node_text_angle = 60)
 ggsave(plot = p, device = "png", width=20, height=10, dpi=300, path = paste0(path, "/Data/clustering_plots/"),
-       filename = paste(date, "_clustree_19_39_48.png", sep = ""))
+       filename = paste(date, "_clustree_21_40_49.png", sep = ""))
 
